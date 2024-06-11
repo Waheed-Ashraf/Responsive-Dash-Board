283d1440752935a398386f68cb399173f902942f
waheed_ashrf <washraf124@gmail.com>
2024-06-12 01:52:50 +0300

    tablet view done

--- lib/widgets/dashboard_mobile_layout.dart
+++ lib/widgets/dashboard_mobile_layout.dart
@@ -1,16 +1,44 @@
 import 'package:flutter/material.dart';
-import 'package:responsive_dash_board/utils/app_styles.dart';
+import 'package:responsive_dash_board/widgets/all_expenses.dart';
+import 'package:responsive_dash_board/widgets/custom_container.dart';
+import 'package:responsive_dash_board/widgets/income_section.dart';
+import 'package:responsive_dash_board/widgets/my_card_section.dart';
+import 'package:responsive_dash_board/widgets/quick_invoice.dart';
+import 'package:responsive_dash_board/widgets/transaction_history_section.dart';
 
 class DashBoardMobileLayout extends StatelessWidget {
   const DashBoardMobileLayout({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text(
-        "Mobile Dash Board ",
-        style: AppStyles.styleSemiBold20(context),
-      ),
+    return const SingleChildScrollView(
+      child: Column(children: [
+        AllExpenses(),
+        Padding(
+          padding: EdgeInsets.symmetric(horizontal: 24),
+          child: QuickInvoice(),
+        ),
+        Padding(
+          padding: EdgeInsets.only(bottom: 24, right: 24, left: 24),
+          child: CustomContainer(
+            padding: 0,
+            child: Column(
+              children: [
+                MyCardSection(),
+                Divider(
+                  height: 40,
+                  color: Color(0xffF1F1F1),
+                ),
+                TransactionHistorySection(),
+              ],
+            ),
+          ),
+        ),
+        Padding(
+          padding: EdgeInsets.only(left: 24),
+          child: IncomeSection(),
+        ),
+      ]),
     );
   }
 }
